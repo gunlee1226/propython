e6e75dca9f88c83c258682e256ffc782e0fe6977
gunlee <gunlee1226>
2020-12-15 15:41:41 +0900

    title/content/ recipes info

---
+++ recipes_title_content.py
@@ -0,0 +1,29 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import re
+
+req = urlopen('https://www.10000recipe.com/recipe/6846168')
+
+print(req.getcode())
+
+if req.getcode() == 200:
+    html = req.read()
+    #print(html)
+
+    html = html.decode("utf-8")
+    #print(html)
+else:
+    print("HTTP ERROR")
+
+soup = BeautifulSoup(html, "html.parser")
+
+r_title = soup.select_one('#contents_area > div.view2_summary.st3 > h3')
+print('요리: ',r_title.get_text())
+
+for i in range(1,15):
+    r_content = soup.select_one('#stepDiv{}'.format(i)).get_text()
+    print('조리방법 :' ,r_content)
+
+for j in range(1,4):
+    r_info = soup.select_one('#contents_area > div.view2_summary.st3 > div.view2_summary_info > span.view2_summary_info{}'.format(j))
+    print('조리정보 :' , r_info.get_text())
